e89461a9ddd5e3db454b432a1fc3e1ce2e17c09b
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-10-01 14:31:51 -0300

    Criando layout e View de criar novas notas

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ const bodyParser = require("body-parser")
 const app = express()
 const port = 8000
 
+//Importação das rotas
+const notesRouter = require('./routes/notes')
+
+// Template Engine
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
 app.get('/', function(req, res){
-	res.send("Aplicação está funcionando")
+	res.render('home')
 })
+
+app.use('/notes', notesRouter)
 
 app.listen(port, () => {
 	console.log("Projeto rodando na porta " + port)
